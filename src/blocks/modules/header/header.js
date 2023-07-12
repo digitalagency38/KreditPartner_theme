354480d2d1f31cde3d830b35ecc35938e9dadfc9
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-07-12 10:41:47 +0500

    Страница 404

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -14,48 +14,8 @@ const Header = class Header {
             this.closeMobileMenu();
         }) 
     }
-    toggleMenu() {
-        document.addEventListener('DOMContentLoaded', function() {
-            const menuItemsWithChildren = document.querySelectorAll('.menu-item-has-children > a');
-        
-            function toggleSubMenu(e) {
-                e.preventDefault();
-                const subMenu = this.nextElementSibling;
-                const isSubMenuVisible = subMenu.style.display === 'block';
-        
-                // Показать/скрыть подменю
-                subMenu.style.display = isSubMenuVisible ? 'none' : 'block';
-        
-                // Если подменю видимо, выполнить переход по ссылке при следующем клике
-                if (isSubMenuVisible) {
-                    this.removeEventListener('click', toggleSubMenu);
-                }
-            }
-        
-            function handleResize() {
-                const screenWidth = window.innerWidth;
-        
-                if (screenWidth <= 1023) {
-                    menuItemsWithChildren.forEach(function(menuItem) {
-                        menuItem.addEventListener('click', toggleSubMenu);
-                    });
-                } else {
-                    menuItemsWithChildren.forEach(function(menuItem) {
-                        menuItem.removeEventListener('click', toggleSubMenu);
-                        const subMenu = menuItem.nextElementSibling;
-                        subMenu.style.display = 'none';
-                    });
-                }
-            }
-        
-            handleResize(); // Вызвать функцию при загрузке страницы
-        
-            window.addEventListener('resize', handleResize); // Слушать событие изменения размера окна
-        });        
-    }
     init() {
         this.setEventListener();
-        this.toggleMenu();
     }
 }
 
