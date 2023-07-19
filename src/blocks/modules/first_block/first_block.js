b6060ade76ffe616f11a519333f8d7a086ff02ce
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-07-19 16:45:24 +0500

    Доработки страниц

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -6,21 +6,23 @@ const FirstBlock = class FirstBlock {
     initSlider() {
         document.addEventListener('DOMContentLoaded', () => {
           const glideCount = document.querySelector('.glide__count');
-          const glide = new Glide('.first_glide_js', {
-            type: 'carousel',
-            perView: 1,
-            gap: 0,
-            startAt: 0
-          });
-      
-          const updateGlideCount = () => {
-            const currentSlide = glide.index + 1;
-            glideCount.textContent = `${currentSlide}`;
-          };
-      
-          glide.on(['mount.after', 'run'], updateGlideCount);
-      
-          glide.mount();
+          if (document.querySelector('.first_glide_js')) {
+            const glide = new Glide('.first_glide_js', {
+              type: 'carousel',
+              perView: 1,
+              gap: 0,
+              startAt: 0
+            });
+        
+            const updateGlideCount = () => {
+              const currentSlide = glide.index + 1;
+              glideCount.textContent = `${currentSlide}`;
+            };
+        
+            glide.on(['mount.after', 'run'], updateGlideCount);
+        
+            glide.mount();
+          }
         });
     }
       
