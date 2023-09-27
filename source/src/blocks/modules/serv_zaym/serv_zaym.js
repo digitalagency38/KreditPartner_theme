11f33356bd90179765a926f3204bc52f41f23794
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-09-27 09:54:25 +0500

    Доработки блоков

--- source/src/blocks/modules/serv_zaym/serv_zaym.js
+++ source/src/blocks/modules/serv_zaym/serv_zaym.js
@@ -1,42 +1,39 @@
 const ServZaym = class ServZaym {
-    constructor() {}
-  
-    openQuestion() {
-        document.addEventListener('DOMContentLoaded', () => {
-            let questions = document.querySelectorAll('.serv_zaym__block');
+  constructor() {}
 
-            questions.forEach(question => {
-              const title = question.querySelector('.serv_zaym__block--top');
-              const text = question.querySelector('.serv_zaym__content');
-            
-              title.addEventListener('click', () => {
-                const isActive = title.classList.contains('isActive');
-                const isShow = text.classList.contains('isShow');
-            
-                questions.forEach(q => {
-                  const qText = q.querySelector('.serv_zaym__content');
-                  const qTitle = q.querySelector('.serv_zaym__block--top');
-                  qText.style.maxHeight = null;
-                  qTitle.classList.remove('isActive');
-                  qText.classList.remove('isShow');
-                });
-            
-                if (!isActive && !isShow) {
-                  // text.style.maxHeight = (text.scrollHeight + 5) + 'px';
-                  title.classList.add('isActive');
-                  text.classList.add('isShow');
-                }
-              });
-            });
-            
-            
+  openQuestion() {
+    document.addEventListener('DOMContentLoaded', () => {
+      let questions = document.querySelectorAll('.serv_zaym__block');
+
+      questions.forEach(question => {
+        const title = question.querySelector('.serv_zaym__block--top');
+        const text = question.querySelector('.serv_zaym__content');
+
+        title.addEventListener('click', () => {
+          const isActive = title.classList.contains('isActive');
+          const isShow = text.classList.contains('isShow');
+
+          questions.forEach(q => {
+            const qText = q.querySelector('.serv_zaym__content');
+            const qTitle = q.querySelector('.serv_zaym__block--top');
+            qText.style.maxHeight = null;
+            qTitle.classList.remove('isActive');
+            qText.classList.remove('isShow');
+          });
+
+          if (!isActive && !isShow) {
+            text.style.maxHeight = (text.scrollHeight + 80) + 'px'; // Учтите паддинг
+            title.classList.add('isActive');
+            text.classList.add('isShow');
+          }
         });
-    }
-  
-    init() {
-      this.openQuestion();
-    }
-  };
-  
-  export default ServZaym;
-  
+      });
+    });
+  }
+
+  init() {
+    this.openQuestion();
+  }
+};
+
+export default ServZaym;
